aab2bdafb8167886ddc967d2e40939f1bb5bc1bf
Gabe Fierro <gtfierro225@gmail.com>
2016-01-17 12:00:46 -0800

    Add gulp task to load BulidingProfile git repo
    Upon loading the gulp task, this will pull the git repo for the
    BuildingProfile, load the YAML into JSON and save the files. The app
    will be rewritten to include these JSON files (which can be public, so
    we can do this all client-side) rather than having the BuildingProfile
    hardcoded

--- gulp/tasks/build.js
+++ gulp/tasks/build.js
@@ -1,3 +1,3 @@
 var gulp = require('gulp');
 
-gulp.task('build', ['browserify', 'markup']);
+gulp.task('build', ['loadProfile', 'browserify', 'markup']);
---
+++ gulp/tasks/loadProfile.js
@@ -0,0 +1,39 @@
+var gulp = require('gulp');
+var fs = require('fs');
+var git = require('gulp-git');
+var handleErrors = require('../util/handleErrors');
+var yaml = require('js-yaml');
+
+gulp.task('loadProfile', function() {
+    fs.access('./BuildingProfile', fs.F_OK, function(err) {
+        if (err) { // doesn't exist?
+            // git clone it
+            git.clone('https://github.com/gtfierro/BuildingProfile', null, function(err) {
+                if (err) {handleErrors(err);}
+                return;
+            });
+        }
+        // now read the files
+        var domainList = [];
+        yaml.safeLoadAll(fs.readFileSync('./BuildingProfile/domains.yaml'), function(doc) {
+            domainList.push(doc);
+        });
+        var err = fs.writeFileSync("./src/www/domains.json", JSON.stringify(domainList));
+        if (err) {
+            return console.log(err);
+        }
+
+        var tagsList = [];
+        yaml.safeLoadAll(fs.readFileSync('./BuildingProfile/tags.yaml'), function(doc) {
+            tagsList.push(doc);
+        });
+        var err = fs.writeFileSync("./src/www/tags.json", JSON.stringify(tagsList));
+        if (err) {
+            return console.log(err);
+        }
+    });
+    // pull the git repo for building profile ontology
+    // transform the YAML file into JSON
+    // save the JSON file in the config
+    // have the react code read in the config file instead of hardcoding it
+});
